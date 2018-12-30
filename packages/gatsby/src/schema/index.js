9636d2af886d79bd7fb154aeea56b1aee7d08d0d
Stefan Probst <stefanprobst@kolabnow.com>
2018-12-30 21:01:08 +0100

    Disable mergeSchemas for now

--- packages/gatsby/src/schema/index.js
+++ packages/gatsby/src/schema/index.js
@@ -1,4 +1,4 @@
-const { mergeSchemas } = require(`graphql-tools`)
+// const { mergeSchemas } = require(`graphql-tools`)
 // const tracer = require(`opentracing`).globalTracer()
 
 const { buildSchema, updateSchema } = require(`./schema`)
@@ -8,18 +8,31 @@ module.exports = async ({ parentSpan, update }) => {
   // const spanArgs = parentSpan ? { childOf: parentSpan } : {}
   // const span = tracer.startSpan(`build schema`, spanArgs)
 
-  const thirdPartySchemas = store.getState().thirdPartySchemas || []
+  // const thirdPartySchemas = store.getState().thirdPartySchemas || []
   const gatsbySchema = update ? await updateSchema() : await buildSchema()
 
   // span.finish()
 
   // TODO: Investigate alternatives to `graphql-tools`'s `mergeSchemas()`,
-  // which seems to touch every field resolver (which is why we have to call
-  // resolvers with `fieldNodes` etc. in `get-nodes-for-query`).
-  // Also: do we lose custom directives when merging???
-  const schema = mergeSchemas({
-    schemas: [gatsbySchema, ...thirdPartySchemas],
-  })
+  // which seems to wrap every field resolver twice!!
+  //   - which is why we need `fieldNodes` argument in `get-nodes-for-query`
+  //   - and it makes the link resolvers not work (they are not on the fields)
+  //     instead we have (i) one resolver wrapper that adds info.mergeInfo,
+  //     and then the defaultMergedResolver
+  //  - also: merge-schemas does not seem to copy over the `GraphQLDirective`s.
+  // Result: everything breaks.
+  // FIXME: If we have to add visitors here, then don't add them in getSchema()
+  // TODO: Best would be to get rid of updateSchema(), and just merge once.
+  // OR: instead of mergeSchemas, just add the thirdparty schemas on a field
+  // on schemaComposer.Query before getSchema() -- which would work, except graphql
+  // checks if `isDeprecated` is present on the field and throws if it is.
+  // The official position is that it is not supported to add fields to the schema
+  // that you get with getFields() because those are a runtime representation.
+  // OTOF it *might* work, if there was a `_fields` present instead of `_gqfields`
+  // const schema = mergeSchemas({
+  //   schemas: [gatsbySchema, ...thirdPartySchemas],
+  // })
+  const schema = gatsbySchema
 
   store.dispatch({
     type: `SET_SCHEMA`,
