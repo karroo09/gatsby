1f2ae2794a0d576544205ef5a24e7e3a6c78bb30
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-09 14:09:32 +0100

    Use getTypes

--- packages/gatsby/src/schema/infer/index.js
+++ packages/gatsby/src/schema/infer/index.js
@@ -1,10 +1,9 @@
 const { schemaComposer } = require(`graphql-compose`)
 
-const { getNodes, getNodesByType } = require(`../db`)
+const { getTypes, getNodesByType } = require(`../db`)
 const { getExampleValue } = require(`./example-value`)
 const { addNodeInterface, getNodeInterfaceFields } = require(`../interfaces`)
 const { addInferredFields } = require(`./infer`)
-const { getUniqueValues } = require(`../utils`)
 
 const addInferredType = typeName => {
   const exampleValue = getExampleValue({
@@ -12,17 +11,18 @@ const addInferredType = typeName => {
     typeName,
     ignoreFields: getNodeInterfaceFields(),
   })
+
   const tc = schemaComposer.getOrCreateTC(typeName, tc => {
     addNodeInterface(tc)
   })
-  // getNodeInterfaceFields().forEach(fieldName => delete exampleValue[fieldName])
+
   addInferredFields(tc, exampleValue, typeName)
   return tc
 }
 
 const addInferredTypes = () => {
-  const typeNames = getUniqueValues(getNodes().map(node => node.internal.type))
-  // TODO: return typeNames.map(addInferredType) ?
+  const typeNames = getTypes()
+  // TODO: Filter out ignoreType
   typeNames.forEach(addInferredType)
 }
 
