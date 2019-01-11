e80a34cdeaa934320c4644b52a5320a47506bdb3
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-11 16:41:14 +0100

    Handle double escaped regex strings

--- packages/gatsby/src/schema/utils/string-to-regexp.js
+++ packages/gatsby/src/schema/utils/string-to-regexp.js
@@ -6,7 +6,15 @@ const stringToRegExp = str => {
     lastSlash > 0 && str.startsWith(`/`),
     `Regular expressions have to be surrounded by slashes, e.g. /\\.txt$/i`
   )
-  return new RegExp(str.slice(1, lastSlash), str.slice(lastSlash + 1))
+  return new RegExp(
+    str
+      .slice(1, lastSlash)
+      // Double escaping is needed to get past the GraphQL parser,
+      // but single escaping is needed for the RegExp constructor,
+      // i.e. `"\\\\w+"` for `/\w+/`.
+      .replace(/\\\\/, `\\`),
+    str.slice(lastSlash + 1)
+  )
 }
 
 module.exports = stringToRegExp
