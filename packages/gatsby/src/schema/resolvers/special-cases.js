b93e38388576a9157853ab5e1936d54195e7d751
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-13 18:37:17 +0100

    Normalize path inputs

--- packages/gatsby/src/schema/resolvers/special-cases.js
+++ packages/gatsby/src/schema/resolvers/special-cases.js
@@ -1,3 +1,5 @@
+const slash = require(`slash`)
+
 const {
   getBaseDir,
   getComponentDir,
@@ -5,17 +7,16 @@ const {
   withBaseDir,
 } = require(`../utils`)
 
-const convert = (relativePath, baseDir) => {
-  const withDir = withBaseDir(baseDir)
-  switch (typeof relativePath) {
+const convert = (path, transform = slash) => {
+  switch (typeof path) {
     case `string`:
-      return withDir(relativePath)
+      return transform(path)
     case `object`:
-      if (Array.isArray(relativePath)) {
-        return relativePath.map(withDir)
-      } else if (relativePath) {
-        return Object.entries(relativePath).reduce((acc, [operator, p]) => {
-          acc[operator] = convert(p, baseDir)
+      if (Array.isArray(path)) {
+        return path.map(transform)
+      } else if (path) {
+        return Object.entries(path).reduce((acc, [operator, p]) => {
+          acc[operator] = convert(p, transform)
           return acc
         }, {})
       }
@@ -23,44 +24,50 @@ const convert = (relativePath, baseDir) => {
   return null
 }
 
-const toAbsolutePath = (relativePath, source, isRootQuery) => {
-  const baseDir = isRootQuery
-    ? getComponentDir(source)
-    : getBaseDir(getParentNode(source))
-  return baseDir && convert(relativePath, baseDir)
-}
+const getDir = (source, isRootQuery) =>
+  isRootQuery ? getComponentDir(source) : getBaseDir(getParentNode(source))
 
 const withSpecialCases = ({ type, source, args, context, info }) => {
   switch (type) {
     case `File`:
-      if (args.filter && source) {
-        // TODO: slash(args.filter.absolutePath)
-        // TODO: slash(args.filter.relativePath)
-        // TODO: slash(args.filter.absoluteDirectory)
-        // TODO: slash(args.filter.relativeDirectory)
+      if (args.filter) {
+        if (args.filter.absolutePath) {
+          args.filter.absolutePath = convert(args.filter.absolutePath)
+        }
+        if (args.filter.absoluteDirectory) {
+          args.filter.absoluteDirectory = convert(args.filter.absoluteDirectory)
+        }
         if (args.filter.relativePath) {
-          const absolutePath = toAbsolutePath(
-            args.filter.relativePath,
-            source,
-            // FIXME: For now, keep v2 behavior
-            false
-            // info.parentType && info.parentType.name === `Query`
-          )
-          if (absolutePath) {
+          const { relativePath } = args.filter
+          // FIXME: For now, keep v2 behavior
+          const baseDir = source && getDir(source, false)
+          // const baseDir =
+          //   source &&
+          //   getDir(source, info.parentType && info.parentType.name === `Query`)
+          if (baseDir) {
+            args.filter.absolutePath = convert(
+              relativePath,
+              withBaseDir(baseDir)
+            )
             delete args.filter.relativePath
-            args.filter.absolutePath = absolutePath
+          } else {
+            args.filter.relativePath = convert(relativePath)
           }
         } else if (args.filter.relativeDirectory) {
-          const absoluteDirectory = toAbsolutePath(
-            args.filter.relativeDirectory,
-            source,
-            // FIXME: For now, keep v2 behavior
-            false
-            // info.parentType && info.parentType.name === `Query`
-          )
-          if (absoluteDirectory) {
+          const { relativeDirectory } = args.filter
+          // FIXME: For now, keep v2 behavior
+          const baseDir = source && getDir(source, false)
+          // const baseDir =
+          //   source &&
+          //   getDir(source, info.parentType && info.parentType.name === `Query`)
+          if (baseDir) {
+            args.filter.absoluteDirectory = convert(
+              relativeDirectory,
+              withBaseDir(baseDir)
+            )
             delete args.filter.relativeDirectory
-            args.filter.absoluteDirectory = absoluteDirectory
+          } else {
+            args.filter.relativeDirectory = convert(relativeDirectory)
           }
         }
       }
