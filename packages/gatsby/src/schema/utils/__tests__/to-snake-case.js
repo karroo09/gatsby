8272928ce0d825bb8d537eb175395d27bc0bbc04
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-15 17:03:37 +0100

    Fix snakecasing

--- packages/gatsby/src/schema/utils/__tests__/to-snake-case.js
+++ packages/gatsby/src/schema/utils/__tests__/to-snake-case.js
@@ -8,6 +8,13 @@ describe(`toSnakeCase util`, () => {
     expect(snakeCased).toBe(expected)
   })
 
+  it(`snakecases multiple uppercase characters`, () => {
+    const str = `FooBarBAZ`
+    const snakeCased = toSnakeCase(str)
+    const expected = `Foo_bar_baz`
+    expect(snakeCased).toBe(expected)
+  })
+
   it(`handles null`, () => {
     const str = null
     const snakeCased = toSnakeCase(str)
