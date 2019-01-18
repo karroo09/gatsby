ba5ad8f8683197b06078a6436efac2526e37119a
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-18 14:26:55 +0100

    Add test for regex string as GraphQL arg

--- packages/gatsby/src/schema/utils/__tests__/string-to-regexp.js
+++ packages/gatsby/src/schema/utils/__tests__/string-to-regexp.js
@@ -22,4 +22,39 @@ describe(`Convert string to RegExp`, () => {
     expect(() => stringToRegExp(`i/\\w+/`)).toThrow()
     expect(() => stringToRegExp(`/\\w+/z`)).toThrow()
   })
+
+  it(`handles regex string passed as graphql arg`, async () => {
+    const {
+      GraphQLSchema,
+      GraphQLObjectType,
+      GraphQLString,
+      graphql,
+    } = require(`graphql`)
+    const QueryType = new GraphQLObjectType({
+      name: `Query`,
+      fields: {
+        regex: {
+          type: GraphQLString,
+          args: {
+            regex: { type: GraphQLString },
+          },
+          resolve: (source, args) => args.regex,
+        },
+      },
+    })
+    const schema = new GraphQLSchema({ query: QueryType })
+    const results = await graphql(
+      schema,
+      `
+        {
+          regex(regex: "/\\\\w+/")
+        }
+      `
+    )
+    expect(results.errors).toBeUndefined()
+    expect(results.data).toEqual({ regex: `/\\w+/` })
+
+    const expected = /\w+/
+    expect(stringToRegExp(results.data.regex)).toEqual(expected)
+  })
 })
