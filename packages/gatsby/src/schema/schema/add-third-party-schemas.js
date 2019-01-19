343afa2c23a247259ded09ab43ec6ef3d21adecc
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-19 18:22:44 +0100

    Merge types from third-party schema

--- packages/gatsby/src/schema/schema/add-third-party-schemas.js
+++ packages/gatsby/src/schema/schema/add-third-party-schemas.js
@@ -5,8 +5,18 @@ const { store } = require(`../../redux`)
 const addThirdPartySchemas = () => {
   const schemas = store.getState().thirdPartySchemas
   schemas.forEach(schema => {
-    const QueryTC = TypeComposer.create(schema.getQueryType())
-    schemaComposer.Query.addFields(QueryTC.getFields())
+    const QueryTC = TypeComposer.createTemp(schema.getQueryType())
+    const fields = QueryTC.getFields()
+    schemaComposer.Query.addFields(fields)
+
+    // Explicitly add the third-party schema's types, so they can be targeted
+    // in `addResolvers` API.
+    const rootTypeName = Object.values(fields)[0].type.name
+    const types = schema.getTypeMap()
+    Object.entries(types).forEach(
+      ([typeName, type]) =>
+        typeName.startsWith(rootTypeName) && schemaComposer.add(type)
+    )
   })
 }
 
