676518ec421d677d0117fe13d46606f9faab3492
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-20 22:19:55 +0100

    Improve error message

--- packages/gatsby/src/schema/infer/infer.js
+++ packages/gatsby/src/schema/infer/infer.js
@@ -130,9 +130,9 @@ const addInferredFields = (tc, obj, prefix, depth = 0) => {
       // Don't create a field with the sanitized key if a field with that name already exists
       invariant(
         obj[key] == null && !tc.hasField(key),
-        `Invalid key ${selector}. GraphQL field names must only contain ` +
-          `characters matching /^[a-zA-Z][_a-zA-Z0-9]*$/. and must not start ` +
-          `with a double underscore.`
+        `Invalid key ${unsanitizedKey} on ${prefix}. GraphQL field names must ` +
+          `only contain characters matching /^[a-zA-Z][_a-zA-Z0-9]*$/. and ` +
+          `must not start with a double underscore.`
       )
 
       const resolver = fieldConfig.resolve || defaultFieldResolver
