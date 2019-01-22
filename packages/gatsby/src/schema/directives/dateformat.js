f3a99a7e461e1171830f814533263653e95918ab
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-22 08:11:49 +0100

    Make @dateformat handle arrays

--- packages/gatsby/src/schema/directives/dateformat.js
+++ packages/gatsby/src/schema/directives/dateformat.js
@@ -4,6 +4,8 @@ const {
   GraphQLDirective,
   GraphQLString,
   GraphQLBoolean,
+  GraphQLList,
+  GraphQLNonNull,
 } = require(`graphql`)
 const { SchemaDirectiveVisitor } = require(`graphql-tools`)
 const {
@@ -18,16 +20,24 @@ const {
 // formats but toISOString (could use utils/isDate)
 // TODO: `difference` arg should be GraphQLDate?
 
+const convertFormatString = str =>
+  // `date-fns` uses a more standard compliant format than `momentjs`, e.g.
+  // `DD` refers to day of year, not day of month (this would be `dd`).
+  // This would be a breaking change, so we emulate the old behavior here.
+  // @see https://git.io/fxCyr
+  str && str.replace(/D/g, `d`).replace(/Y/g, `y`)
+
 const toDate = date => (typeof date === `string` ? parseISO(date) : date)
 
 const formatDate = (
   dateOrString,
-  formatString,
+  momentFormatString,
   lang,
   timeZone,
   fromNow,
   difference
 ) => {
+  const formatString = convertFormatString(momentFormatString)
   const locale = lang && require(`date-fns/locale/${lang}`)
   const date = toDate(dateOrString)
   if (fromNow) {
@@ -40,9 +50,6 @@ const formatDate = (
   }
   return format(date, formatString, {
     locale,
-    // Allow formatting with YYYY-MM-DD, instead of the more correct yyyy-MM-dd
-    // @see: https://git.io/fxCyr
-    awareOfUnicodeTokens: true,
     // timeZone, // IANA time zone, needs `date-fns-tz`
   })
 }
@@ -89,19 +96,31 @@ class DateFormatDirectiveVisitor extends SchemaDirectiveVisitor {
         ...rest
       } = args
       const date = await resolve(source, rest, context, info)
-      return date
-        ? formatDate(
-            date,
-            formatString || defaultFormatString,
-            locale || defaultLocale,
-            timeZone || defaultTimeZone,
-            fromNow !== undefined ? fromNow : defaultFromNow,
-            difference !== undefined ? difference : defaultDifference
-          )
-        : null
+      const format = date =>
+        formatDate(
+          date,
+          formatString || defaultFormatString,
+          locale || defaultLocale,
+          timeZone || defaultTimeZone,
+          fromNow !== undefined ? fromNow : defaultFromNow,
+          difference !== undefined ? difference : defaultDifference
+        )
+      return Array.isArray(date) ? date.map(format) : date ? format(date) : null
     }
 
-    field.type = GraphQLString
+    const wrappers = []
+    let fieldType = field.type
+    while (
+      fieldType instanceof GraphQLList ||
+      fieldType instanceof GraphQLNonNull
+    ) {
+      wrappers.unshift(fieldType.constructor)
+      fieldType = fieldType.ofType
+    }
+    field.type = wrappers.reduce(
+      (type, Wrapper) => new Wrapper(type),
+      GraphQLString
+    )
   }
 }
 
