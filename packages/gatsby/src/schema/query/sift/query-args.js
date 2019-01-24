77983c97cb4598ec5af199169369f8e379351bd0
Stefan Probst <stefanprobst@kolabnow.com>
2019-01-24 15:26:21 +0100

    Add elemMatch operator

--- packages/gatsby/src/schema/query/sift/query-args.js
+++ packages/gatsby/src/schema/query/sift/query-args.js
@@ -6,6 +6,9 @@ const { isObject, stringToRegExp } = require(`../../utils`)
 const prepareQueryArgs = filterFields =>
   Object.entries(filterFields).reduce((acc, [key, value]) => {
     if (isObject(value)) {
+      if (key === `elemMatch`) {
+        acc[`$elemMatch`] = prepareQueryArgs(value)
+      }
       acc[key] = prepareQueryArgs(value)
     } else {
       switch (key) {
@@ -24,7 +27,10 @@ const prepareQueryArgs = filterFields =>
 
 const dropQueryOperators = filterFields =>
   Object.entries(filterFields).reduce((acc, [key, value]) => {
-    if (isObject(value) && isObject(Object.values(value)[0])) {
+    if (key === `elemMatch`) {
+      const [k, v] = Object.entries(value)[0]
+      acc[k] = dropQueryOperators(v)
+    } else if (isObject(value) && isObject(Object.values(value)[0])) {
       acc[key] = dropQueryOperators(value)
     } else {
       acc[key] = true
