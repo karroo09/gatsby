d4eded7e9428d6ba807c9cbfdade9c8b0e38dddf
Stefan Probst <stefanprobst@kolabnow.com>
2019-02-05 21:58:45 +0100

    [compatibility] Sanitize field names like in v2

--- packages/gatsby/src/schema/utils/__tests__/create-field-name.js
+++ packages/gatsby/src/schema/utils/__tests__/create-field-name.js
@@ -1,5 +1,50 @@
 const createFieldName = require(`../create-field-name`)
 
+// describe(`createFieldName util`, () => {
+//   it(`creates a valid field name from a key with spaces`, () => {
+//     const key = `foo bar baz`
+//     const fieldName = createFieldName(key)
+//     const expected = `foo_bar_baz`
+//     expect(fieldName).toBe(expected)
+//   })
+
+//   it(`creates a valid field name from a key starting with a digit`, () => {
+//     const key = `0foo bar..baz`
+//     const fieldName = createFieldName(key)
+//     const expected = `_foo_bar__baz`
+//     expect(fieldName).toBe(expected)
+//   })
+
+//   it(`creates a valid field name from a key with unsafe characters`, () => {
+//     const key = `00^°"§$%&/()=?{}[]\`´\\+*~#'-_:;,<>|`
+//     const fieldName = createFieldName(key)
+//     const expected = `_0________________________________`
+//     expect(fieldName).toBe(expected)
+//   })
+
+//   it(`creates a valid field name from a key with emoji`, () => {
+//     const key = `foo🐱bar`
+//     const fieldName = createFieldName(key)
+//     const expected = `foo__bar`
+//     expect(fieldName).toBe(expected)
+//   })
+
+//   it(`throws on key with leading double underscore`, () => {
+//     const key = `__foobar`
+//     expect(() => createFieldName(key)).toThrow()
+//   })
+
+//   it(`throws on key with two leading unsafe chars`, () => {
+//     const key = `0$foobar`
+//     expect(() => createFieldName(key)).toThrow()
+//   })
+
+//   it(`throws on null`, () => {
+//     const key = null
+//     expect(() => createFieldName(key)).toThrow()
+//   })
+// })
+
 describe(`createFieldName util`, () => {
   it(`creates a valid field name from a key with spaces`, () => {
     const key = `foo bar baz`
@@ -11,14 +56,14 @@ describe(`createFieldName util`, () => {
   it(`creates a valid field name from a key starting with a digit`, () => {
     const key = `0foo bar..baz`
     const fieldName = createFieldName(key)
-    const expected = `_foo_bar__baz`
+    const expected = `_0foo_bar__baz`
     expect(fieldName).toBe(expected)
   })
 
   it(`creates a valid field name from a key with unsafe characters`, () => {
     const key = `00^°"§$%&/()=?{}[]\`´\\+*~#'-_:;,<>|`
     const fieldName = createFieldName(key)
-    const expected = `_0________________________________`
+    const expected = `_00________________________________`
     expect(fieldName).toBe(expected)
   })
 
@@ -29,14 +74,18 @@ describe(`createFieldName util`, () => {
     expect(fieldName).toBe(expected)
   })
 
-  it(`throws on key with leading double underscore`, () => {
-    const key = `__foobar`
-    expect(() => createFieldName(key)).toThrow()
+  it(`sanitizes key with leading double underscore`, () => {
+    const key = `__fööbar`
+    const fieldName = createFieldName(key)
+    const expected = `_xfxxbar`
+    expect(fieldName).toBe(expected)
   })
 
-  it(`throws on key with two leading unsafe chars`, () => {
+  it(`sanitizes key with two leading unsafe chars`, () => {
     const key = `0$foobar`
-    expect(() => createFieldName(key)).toThrow()
+    const fieldName = createFieldName(key)
+    const expected = `_0_foobar`
+    expect(fieldName).toBe(expected)
   })
 
   it(`throws on null`, () => {
--- packages/gatsby/src/schema/utils/create-field-name.js
+++ packages/gatsby/src/schema/utils/create-field-name.js
@@ -1,8 +1,29 @@
-const { assertValidName } = require(`graphql`)
+// const { assertValidName } = require(`graphql`)
+
+// const createFieldName = str => {
+//   const name = str.replace(/^\d|[^\w]/g, `_`)
+//   assertValidName(name)
+
+//   return name
+// }
+
+const invariant = require(`invariant`)
 
 const createFieldName = str => {
-  const name = str.replace(/^\d|[^\w]/g, `_`)
-  assertValidName(name)
+  const name = str.replace(/[^\w]/g, `_`)
+
+  invariant(
+    typeof str === `string`,
+    `GraphQL field name ${str} is not a string.`
+  )
+
+  if (name.match(/^__/)) {
+    return name.replace(/_/g, (char, index) => (index === 0 ? `_` : `x`))
+  }
+
+  if (name.match(/^\d/)) {
+    return `_` + name
+  }
 
   return name
 }
