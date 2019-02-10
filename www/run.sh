821e3687c3775a9d0998632e4be4fbd520b3ac93
Stefan Probst <stefanprobst@kolabnow.com>
2019-02-10 11:53:39 +0100

    Update relay patch

--- www/run.sh
+++ www/run.sh
@@ -10,6 +10,6 @@ gatsby-dev --set-path-to-repo ..
 gatsby-dev --scan-once --packages=gatbsy
 
 # relay-compiler#2638
-patch node_modules/relay-compiler/lib/GraphQLIRPrinter.js relay.patch
+patch node_modules/relay-compiler/lib/GraphQLIRPrinter.js -i relay.patch
 
 GATSBY_SCREENSHOT_PLACEHOLDER=true yarn develop
